87ae5a7f5d264438e1cf637eef40d5f9035b46f2
Octowl <alsaff1987@gmail.com>
2024-10-19 15:23:58 +0300

    book detail page

--- src/app/books/[id]/page.js
+++ src/app/books/[id]/page.js
@@ -1,22 +1,23 @@
 import books from "@/data/books";
 import Image from "next/image";
 
-function Book() {
+function Book({ params }) {
+  const book = books.find(book => book.id === +params.id)
   return (
     <div className="flex gap-3 p-3">
       <div className="w-1/3 flex justify-center">
         <Image
-          src={books[0].image}
-          alt={books[0].bookName}
+          src={book.image}
+          alt={book.bookName}
           width={300}
           height={300}
         />
       </div>
       <div className="w-2/3">
-        <h2>{books[0].bookName}</h2>
-        <h6>Release Year: {books[0].releaseYear}</h6>
+        <h2>{book.bookName}</h2>
+        <h6>Release Year: {book.releaseYear}</h6>
         <p>Description:</p>
-        <p>{books[0].description}</p>
+        <p>{book.description}</p>
       </div>
     </div>
   );
