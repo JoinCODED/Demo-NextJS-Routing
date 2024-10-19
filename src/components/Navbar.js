4430f09941f90ad1745399770026de5cdbf19acf
Octowl <alsaff1987@gmail.com>
2024-10-19 15:34:04 +0300

    link to home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import NavItem from "./NavItem";
 
 function Navbar() {
   return (
     <nav className="flex flex-wrap items-center justify-between mx-auto p-4">
-      <div>
+      <Link href="/">
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/9/9b/HP_-_Harry_Potter_wordmark.svg"
           alt="HP Logo"
@@ -13,7 +14,7 @@ function Navbar() {
           width={80}
           height={80}
         />
-      </div>
+      </Link>
       <ul className="flex justify-end gap-3 list-none">
         <NavItem name="Houses" href="/houses" />
         <NavItem name="Books" href="/books" />
