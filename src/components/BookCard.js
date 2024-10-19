f410a1dd220a9cce365847acdc4e3c76b095ecb1
Octowl <alsaff1987@gmail.com>
2024-10-19 15:28:18 +0300

    links

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function BookCard({ book }) {
   return (
     <div className="flex flex-col justify-center items-center w-1/5 rounded-xl p-3 border border-gray-500 border-solid">
-      <Image
-        src={book.image}
-        alt={book.bookName}
-        width={300}
-        height={300}
-      />
-      <h5>{book.bookName}</h5>
+      <Link href={`/books/${book.slug}`}>
+        <Image
+          src={book.image}
+          alt={book.bookName}
+          width={300}
+          height={300}
+        />
+        <h5>{book.bookName}</h5>
+      </Link>
     </div>
   );
 };
