b1a77d491dd5335ccb53a2b2f4155c56f7dc6897
Octowl <alsaff1987@gmail.com>
2024-10-19 15:10:30 +0300

    use link

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,5 +1,7 @@
-function NavItem({ name }) {
-  return <li className="hover:bg-gray-200 rounded-lg p-3">{name}</li>;
+import Link from "next/link";
+
+function NavItem({ name, href }) {
+  return <Link href={href} className="hover:bg-gray-200 rounded-lg p-3">{name}</Link>;
 };
 
 export default NavItem;
