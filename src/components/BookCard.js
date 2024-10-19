f3e390e4e34a7a84a94a5e4d22037f28b2e1da45
Octowl <alsaff1987@gmail.com>
2024-10-19 15:29:16 +0300

    cleaner link

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,17 +4,15 @@ import React from "react";
 
 function BookCard({ book }) {
   return (
-    <div className="flex flex-col justify-center items-center w-1/5 rounded-xl p-3 border border-gray-500 border-solid">
-      <Link href={`/books/${book.slug}`}>
-        <Image
-          src={book.image}
-          alt={book.bookName}
-          width={300}
-          height={300}
-        />
-        <h5>{book.bookName}</h5>
-      </Link>
-    </div>
+    <Link href={`/books/${book.slug}`} className="flex flex-col justify-center items-center w-1/5 rounded-xl p-3 border border-gray-500 border-solid">
+      <Image
+        src={book.image}
+        alt={book.bookName}
+        width={300}
+        height={300}
+      />
+      <h5>{book.bookName}</h5>
+    </Link>
   );
 };
 
